afe1de74802f71a34f7075b8fe12bb6699f640e1
Pankaj Kumar Malik <pankajkumarmalik6@gmail.com>
2024-01-26 21:07:54 +0530

    2nd commit

--- api/connect.js
+++ api/connect.js
@@ -1,9 +1,13 @@
 import mysql from "mysql";
+import dotenv from"dotenv";
+
+dotenv.config()
+
 
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Serverpk93+",
+    password: process.env.MYSQL_PASSWORD,
     database: "social"
-
+    
 });
